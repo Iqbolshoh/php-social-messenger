3031c733c87add5735b207c12d687027dd49d35a
Iqbolshoh <iilhomjonov777@gmail.com>
2024-11-18 07:19:18 +0500

    commit

--- database.sql
+++ database.sql
@@ -4,6 +4,8 @@ USE SocialChat;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     email VARCHAR(120) NOT NULL UNIQUE,
     username VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
