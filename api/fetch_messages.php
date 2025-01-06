b5fbb8b5776101aca6160bc4bb909d898f52c412
Iqbolshoh <iilhomjonov777@gmail.com>
2025-01-06 13:35:15 +0500

    commit

--- api/fetch_messages.php
+++ api/fetch_messages.php
@@ -21,19 +21,32 @@ if (isset($_POST['id'])) {
     $sender_id = $_SESSION['user_id'];
     $receiver_id = $_POST['id'];
 
-    $updateQuery = "UPDATE messages SET status = 'read' WHERE sender_id = ? AND receiver_id = ? AND status = 'unread'";
-    $query->executeQuery($updateQuery, [$receiver_id, $sender_id], 'ii');
+    $data = ['status' => 'read'];
+    $update_result = $query->update(
+        'messages',
+        $data,
+        'sender_id = ? AND receiver_id = ? AND status = "unread"',
+        [$receiver_id, $sender_id],
+        'ii'
+    );
 
     $response['status'] = 'success';
     $response['message'] = 'Messages fetched successfully';
-
-    $response['data'] = $query->select(
+    $messages = $query->select(
         'messages',
         '*',
-        "((sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)) ORDER BY created_at",
+        "((sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)) 
+         ORDER BY created_at",
         [$sender_id, $receiver_id, $receiver_id, $sender_id],
         "iiii"
     );
+
+    if (!empty($messages)) {
+        $response['data'] = $messages;
+    } else {
+        $response['status'] = 'error';
+        $response['message'] = 'No messages found';
+    }
 } else {
     $response['status'] = 'error';
     $response['message'] = 'Receiver ID is required';
