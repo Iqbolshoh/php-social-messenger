c0464c496d345ff8720baf21ccc7ab35c9ad00db
Iqbolshoh <148738947+Iqbolshoh@users.noreply.github.com>
2025-01-07 17:57:44 +0500

    commit

--- api/auth/logout.php
+++ api/auth/logout.php
@@ -3,6 +3,10 @@ session_start();
 session_unset();
 session_destroy();
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
 $cookies = ['username', 'session_token'];
 foreach ($cookies as $cookie) {
     if (isset($_COOKIE[$cookie])) {
